edd959138731bf1b93f37ac9e01aa43cdd584f8e
jagil_pacifico <jagil@soaint.com>
2025-12-28 13:17:22 -0500

    probando

--- API01/directivo.js
+++ API01/directivo.js
@@ -1,35 +1,28 @@
 import http from 'k6/http';
-import { check } from 'k6';
-import { getBaseUrl } from '../../utils/urlManager.js';
+import { check, fail } from 'k6'; 
+import { getBaseUrl } from './utils/urlManager.js'; 
 
-export let options = {
-  vus: 12,
-  duration: '30s',
-};  
-
-const dniAuxiliar = __ENV.DNI_AUXILIAR;
-
-export function setup() {
-  const token = getToken();
-  return { token: token }; 
-}
-
-export default function (data) {
-  const token = data.token; 
-  
+export function getToken() {
   const baseUrl = getBaseUrl();
-  const getUrl = baseUrl + `/api/auxiliares/${dniAuxiliar}/estado`;
-
-  const body = JSON.stringify({}); 
-
-  const response = http.request('PATCH', getUrl, body, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
+  const loginUrl = baseUrl + '/api/login/directivo';
+  
+  const loginPayload = JSON.stringify({
+    Nombre_Usuario: __ENV.NOMBRE_USUARIO_DIRECTIVO,
+    Contraseña: __ENV.CONTRASENA_DIRECTIVO,
   });
+
+  const headers = { 'Content-Type': 'application/json' };
+  const response = http.post(loginUrl, loginPayload, { headers });
+
+  if (response.status !== 200) {
+    console.error(`❌ Error Login Directivo: ${response.status} - ${response.body}`);
+    fail('No se pudo obtener el token de Directivo'); 
+  }
 
   check(response, {
-    'PATCH request fue exitoso': (r) => r.status === 200,
+    'Login exitoso': (r) => r.status === 200,
   });
+
+  const token = response.json('data.token');
+  return token; 
 }
